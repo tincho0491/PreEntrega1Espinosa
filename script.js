70266d1e14b6ed4be477341f7028969f22640ea1
mespinosa <espinosa_m.i@hotmail.com>
2022-12-23 16:17:27 -0300

    segundo commit

--- script.js
+++ script.js
@@ -1,143 +1,70 @@
 alert("Bienvenido a la sala de espera de la clínica")
 
 let tiempoDeEspera
-let flag = true
 
-if (flag) {
-    while (1) {
-        let turnosRegistrados = prompt("¿Cuántos turnos se han registrado hasta el momento?")
-        if (isNaN(turnosRegistrados) || turnosRegistrados < 0) {
-            alert("Por favor, ingrese un número válido (mayor o igual a 0).")
-        }
-        else {
-            tiempoDeEspera = turnosRegistrados * 15
-            alert("Se han registrado " + turnosRegistrados + " turnos. La espera estimada es de " + tiempoDeEspera + " minutos")
-            flag = false
-            break
-        }
+while (1) {
+    let turnosRegistrados = prompt("¿Cuántos turnos se han registrado hasta el momento?")
+    if (isNaN(turnosRegistrados) || turnosRegistrados < 0) {
+        alert("Por favor, ingrese un número válido (mayor o igual a 0).")
+    }
+    else {
+        tiempoDeEspera = turnosRegistrados * 15
+        alert("Se han registrado " + turnosRegistrados + " turnos. La espera estimada es de " + tiempoDeEspera + " minutos")
+        break
     }
 }
 
 //-----------JUEGO PIEDRA PAPEL O TIJERA----------------
-
-let opcion = prompt("Desea jugar un juego mientras espera? (S/N)").toUpperCase()
 let puntajeUsuario
 let puntajeMaquina
 
-while (opcion == "S") {
-    alert("¡Genial! Vamos a jugar a piedra, papel o tijera. Quien primero gane 3 partidas gana el juego.")
-    puntajeUsuario = 0
-    puntajeMaquina = 0
+while (1) {
+    let opcion = prompt("Desea jugar un juego mientras espera? (S/N)").toUpperCase()
+    while (opcion == "S") {
+        alert("¡Genial! Vamos a jugar a piedra, papel o tijera. Quien primero gane 3 partidas gana el juego.")
+        puntajeUsuario = 0
+        puntajeMaquina = 0
 
-    while (puntajeUsuario < 3 && puntajeMaquina < 3) {
-        let eleccionUsuario = prompt("¿Piedra, papel o tijera? (P/S/T)").toUpperCase()
-        let eleccionMaquinaRandom = Math.floor(Math.random() * 3)
-        let eleccionMaquina
-        switch (eleccionMaquinaRandom) {
-            case 0:
-                eleccionMaquina = "P"
-                break
-            case 1:
-                eleccionMaquina = "T"
-                break
-            case 2:
-                eleccionMaquina = "S"
-                break
+        while (puntajeUsuario < 3 && puntajeMaquina < 3) {
+            let eleccionUsuario = prompt("¿Piedra, papel o tijera? (P/S/T)").toUpperCase()
+            while (eleccionUsuario != "P" && eleccionUsuario != "S" && eleccionUsuario != "T") {
+                alert("Opción inválida. Por favor, ingrese P, S o T para jugar.")
+                eleccionUsuario = prompt("¿Piedra, papel o tijera? (P/S/T)").toUpperCase()
+            }
+            let eleccionMaquinaRandom = Math.floor(Math.random() * 3)
+            let eleccionMaquina
+            switch (eleccionMaquinaRandom) {
+                case 0:
+                    eleccionMaquina = "P"
+                    break
+                case 1:
+                    eleccionMaquina = "T"
+                    break
+                case 2:
+                    eleccionMaquina = "S"
+                    break
+            }
+            let resultado = comprobarRonda(eleccionUsuario, eleccionMaquina)
+            puntaje(resultado)
+            alert("Elegiste " + eleccionUsuario + " y la máquina eligió " + eleccionMaquina + ". " + resultado + "\n\nEl puntaje es: " + puntajeUsuario + " - " + puntajeMaquina)
+
         }
-        let resultado = comprobarRonda(eleccionUsuario, eleccionMaquina)
-        alert("Elegiste " + eleccionUsuario + " y la máquina eligió " + eleccionMaquina + ". " + resultado)
-        puntaje(resultado)
-        alert("El puntaje es: " + puntajeUsuario + " - " + puntajeMaquina)
+        comprobarGanador()
+        opcion = prompt("¿Desea volver a jugar? (S/N)").toUpperCase()
     }
-    comprobarGanador()
-    opcion = prompt("¿Desea volver a jugar? (S/N)").toUpperCase()
-
     if (opcion == "N") {
         alert("¡Gracias por jugar!")
+        break
+    }
+    else if (opcion != "S" && opcion != "N") {
+        alert("Opción inválida. Por favor, ingrese S o N para jugar.")
     }
 }
-while (opcion != "S" && opcion != "N") {
-    alert("Opción inválida. Por favor, ingrese S o N para jugar.")
-    opcion = prompt("Presione S para jugar o N en caso contrario").toUpperCase()
-}
+
+
+
 
 //Reloj Temporizador
-
-horas = new Contador()
-horas.setCuentaInicial(Math.floor(tiempoEnMinutos / 60))
-minutos = new Contador()
-minutos.setCuentaInicial(tiempoEnMinutos % 60)
-segundos = new Contador(0, 1, 60, 0)
-Temporizador()
-
-
-
-//DESARROLLO DE FUNCIONES
-
-function Temporizador() {
-    let intervalo = setInterval(function () {
-        segundos.decrementar()
-        if (segundos.getCuentaActual() == 00) {
-            minutos.decrementar()
-            segundos.setCuentaActual(59)
-        }
-        if (minutos.getCuentaActual() == 00) {
-            horas.decrementar()
-            minutos.setCuentaActual(59)
-        }
-        let tiempo = horas.getCuentaActual() + ":" + minutos.getCuentaActual() + ":" + segundos.getCuentaActual()
-        console.log(tiempo)
-        if (segundos.getCuentaActual() == 00 && minutos.getCuentaActual() == 00 && horas.getCuentaActual() == 00) {
-            clearInterval(intervalo)
-            alert("¡El tiempo de espera ha terminado!")
-        }
-    }, 1000)
-}
-
-function comprobarRonda(Usuario, Maquina) {
-    let resultado
-    if (Usuario == "P" && Maquina == "S") {
-        resultado = "Perdiste"
-    }
-    else if (Usuario == "P" && Maquina == "T") {
-        resultado = "Ganaste"
-    }
-    else if (Usuario == "S" && Maquina == "P") {
-        resultado = "Ganaste"
-    }
-    else if (Usuario == "S" && Maquina == "T") {
-        resultado = "Perdiste"
-    }
-    else if (Usuario == "T" && Maquina == "P") {
-        resultado = "Perdiste"
-    }
-    else if (Usuario == "T" && Maquina == "S") {
-        resultado = "Ganaste"
-    }
-    else {
-        resultado = "Empate"
-    }
-    return resultado
-}
-
-function puntaje(resultado) {
-    if (resultado == "Ganaste") {
-        puntajeUsuario += 1
-    }
-    else if (resultado == "Perdiste") {
-        puntajeMaquina += 1
-    }
-}
-
-function comprobarGanador() {
-    if (puntajeUsuario == 3) {
-        alert("¡Ganaste el juego!")
-    }
-    else if (puntajeMaquina == 3) {
-        alert("¡Perdiste el juego!")
-    }
-}
-
 
 //Clase Contador
 
@@ -149,13 +76,15 @@ class Contador {
         this.maximo = maximo
         this.minimo = minimo
     }
-    constructor() {
+
+    //JavaScript no permite mas de un constructor por clase
+    /* constructor() {
         this.cuentaInicial = 0
         this.cuentaActual = 0
         this.paso = 1
         this.maximo = 100
         this.minimo = 0
-    }
+    } */
 
     incrementar() {
         this.cuentaActual += this.paso
@@ -170,17 +99,18 @@ class Contador {
         }
     }
     decrementar() {
-        this.contador -= this.paso
-        if (this.contador < this.minimo) {
-            this.contador = this.minimo
+        this.cuentaActual -= this.paso
+        if (this.CuentaActual < this.minimo) {
+            this.CuentaActual = this.minimo
         }
     }
     decrementar(n) {
-        this.contador -= n
-        if (this.contador < this.minimo) {
-            this.contador = this.minimo
+        this.cuentaActual -= n
+        if (this.CuentaActual < this.minimo) {
+            this.CuentaActual = this.minimo
         }
     }
+
     //probando funcion flecha
     reset = () => {
         this.cuentaActual = this.cuentaInicial
@@ -224,5 +154,103 @@ class Contador {
 
     getMinimo = () => {
         return this.minimo
+    }
+}
+
+
+/* 
+horas = new Contador(0, 1, 24, 0)
+horas.setCuentaActual(Math.floor(2 / 60))
+minutos = new Contador(0, 1, 60, 0)
+minutos.setCuentaActual(2 % 60)
+segundos = new Contador(0, 1, 60, 0)
+Temporizador()
+
+
+
+//DESARROLLO DE FUNCIONES
+
+function Temporizador() {
+    let intervalo = setInterval(function () {
+        if (segundos.getCuentaActual() == 00 && minutos.getCuentaActual() == 00 && horas.getCuentaActual() == 00) {
+            clearInterval(intervalo)
+            alert("¡El tiempo de espera ha terminado!")
+        }
+        else if (segundos.getCuentaActual() == 00) {
+            if (minutos.getCuentaActual() == 00) {
+                horas.decrementar(1)
+                minutos.setCuentaActual(60)
+            }
+            minutos.decrementar(1)
+            segundos.setCuentaActual(60)
+        }
+
+        segundos.decrementar(1)
+        let tiempo = horas.getCuentaActual() + ":" + minutos.getCuentaActual() + ":" + segundos.getCuentaActual()
+        console.log(tiempo)
+
+    }, 100)
+} */
+
+contador1 = new Contador(9, 1, 10, 0)
+contador1.decrementar()
+cronometro()
+//crear funcion cronometro que incremente hasta 10 cada un segundo
+
+function cronometro() {
+    let intervalo = setInterval(function () {
+        if (contador1.getCuentaActual() == 0) {
+            clearInterval(intervalo)
+        }
+        else {
+            contador1.decrementar()
+            console.log([contador1.getCuentaActual()])
+        }
+    }, 1000)
+}
+
+
+
+function comprobarRonda(Usuario, Maquina) {
+    let resultado
+    if (Usuario == "P" && Maquina == "S") {
+        resultado = "Perdiste"
+    }
+    else if (Usuario == "P" && Maquina == "T") {
+        resultado = "Ganaste"
+    }
+    else if (Usuario == "S" && Maquina == "P") {
+        resultado = "Ganaste"
+    }
+    else if (Usuario == "S" && Maquina == "T") {
+        resultado = "Perdiste"
+    }
+    else if (Usuario == "T" && Maquina == "P") {
+        resultado = "Perdiste"
+    }
+    else if (Usuario == "T" && Maquina == "S") {
+        resultado = "Ganaste"
+    }
+    else {
+        resultado = "Empate"
+    }
+    return resultado
+}
+
+function puntaje(resultado) {
+    if (resultado == "Ganaste") {
+        puntajeUsuario += 1
+    }
+    else if (resultado == "Perdiste") {
+        puntajeMaquina += 1
+    }
+}
+
+function comprobarGanador() {
+    if (puntajeUsuario == 3) {
+        alert("¡Ganaste el juego!")
+    }
+    else if (puntajeMaquina == 3) {
+        alert("¡Perdiste el juego!")
     }
 }
